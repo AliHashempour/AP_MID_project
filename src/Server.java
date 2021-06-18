6448dc8c84f43956e6af766bc29b4ee820b358bf
AliHashempour <ali.hashempour@gmail.com>
2021-06-18 15:53:16 +0430

    commit20

--- src/Server.java
+++ src/Server.java
@@ -66,7 +66,7 @@ public class Server {
             introduction();
 
             Thread.sleep(5000);
-            System.out.println(mafiaNumber + "  " + citizenNumber);
+
             while (!endGame()) {
                 //DAY.........................................................................................
                 dayVoteNight = "day";
